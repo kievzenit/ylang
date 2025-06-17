ee56ebe5226b9de68b57c168b9a399fbfad603ba
kievzenit <kir.backender@gmail.com>
2025-06-17 14:55:50 +0300

    removed SetIsConst method from hir type interface

--- internal/hir/types/type.go
+++ internal/hir/types/type.go
@@ -4,7 +4,6 @@ type Type interface {
 	Type() string
 	SameAs(t Type) bool
 	IsConst() bool
-	SetIsConst(bool)
 	GetMember(string) (Type, bool)
 	CanBeImplicitlyCastedTo(t Type) bool
 	CanBeExplicitlyCastedTo(t Type) bool
